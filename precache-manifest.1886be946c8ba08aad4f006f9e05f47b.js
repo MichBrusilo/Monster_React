4350eaf7be054eeb31b1e8cf2e730a6e3ab95a90
Michał Brusiło <mibru@interia.pl>
2020-04-15 14:57:22 +0200

    Updates

---
+++ precache-manifest.1886be946c8ba08aad4f006f9e05f47b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9f5848b7a3a38fa654057e51a31d9ac",
+    "url": "/monster_react/index.html"
+  },
+  {
+    "revision": "517f1b09da2cdb0ff9df",
+    "url": "/monster_react/static/css/main.6201b893.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/monster_react/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster_react/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "517f1b09da2cdb0ff9df",
+    "url": "/monster_react/static/js/main.e15bdb74.chunk.js"
+  },
+  {
+    "revision": "4d1eb4cfcdadfa5cb577",
+    "url": "/monster_react/static/js/runtime-main.f79b1a69.js"
+  }
+]);
